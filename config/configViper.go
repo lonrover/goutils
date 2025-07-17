a825a93a8685abea44edd64141ff3539db3944b8
xl_bfcec <wuxiaolong@bfcec.com>
2025-07-17 16:18:40 +0800

    添加公共的获取配置文件的方法

--- config/configViper.go
+++ config/configViper.go
@@ -3,8 +3,6 @@ package config
 import (
 	"fmt"
 	"log"
-	"path/filepath"
-	"runtime"
 
 	"github.com/spf13/viper"
 )
@@ -15,18 +13,12 @@ type AppConfig struct {
 }
 
 type DatabaseConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
-	Name     string `mapstructure:"name"`
+	Host          string `mapstructure:"host"`
+	Port          string `mapstructure:"port"`
+	Username      string `mapstructure:"username"`
+	Password      string `mapstructure:"password"`
+	Database_name string `mapstructure:"database_name"`
 }
-
-type RedisConfig struct {
-	Addr     string `mapstructure:"addr"`
-	Password string `mapstructure:"password"`
-}
-
 type LogConfig struct {
 	Path       string `mapstructure:"path"`
 	Level      string `mapstructure:"level"`
@@ -35,29 +27,18 @@ type LogConfig struct {
 }
 
 type Config struct {
-	App      AppConfig      `mapstructure:"app"`
-	Database DatabaseConfig `mapstructure:"database"`
-	Redis    RedisConfig    `mapstructure:"redis"`
-	Log      LogConfig      `mapstructure:"log"`
+	App             AppConfig      `mapstructure:"app"`
+	Mysql_db_config DatabaseConfig `mapstructure:"mysql_db_config"`
+	Log             LogConfig      `mapstructure:"log"`
 }
 
 var GlobalConfig Config
 
 // 初始化配置读取
 func InitConfig() {
-	// 获取当前文件路径（解决工作目录问题）
-	_, b, _, _ := runtime.Caller(0)
-	basePath := filepath.Dir(b)
-
-	viper.SetConfigName("config")      // 配置文件名 (不带扩展名)
-	viper.SetConfigType("yaml")        // 配置文件类型
-	viper.AddConfigPath(basePath)      // 当前文件所在目录
-	viper.AddConfigPath(".")           // 工作目录
-	viper.AddConfigPath("/etc/myapp/") // 全局配置目录
-
-	// 从环境变量读取覆盖配置
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("MYAPP") // 环境变量前缀 MYAPP_DATABASE_PASSWORD
+	viper.SetConfigName("setting")                                // 配置文件名 (不带扩展名)
+	viper.SetConfigType("yaml")                                   // 配置文件类型
+	viper.AddConfigPath("D:/code/work/package_common_go/config/") // 当前文件所在目录WW
 
 	// 读取配置文件
 	if err := viper.ReadInConfig(); err != nil {
@@ -74,8 +55,8 @@ func InitConfig() {
 
 // 获取数据库连接字符串
 func (d *DatabaseConfig) ConnectionString() string {
-	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		d.Host, d.Port, d.Username, d.Password, d.Name)
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		d.Host, d.Port, d.Username, d.Password, d.Database_name)
 }
 
 // 使用示例
@@ -85,10 +66,9 @@ func ExampleUsage() {
 	fmt.Println("Log path:", logPath)
 
 	// 获取数据库连接字符串
-	dbConn := GlobalConfig.Database.ConnectionString()
+	dbConn := GlobalConfig.Mysql_db_config.ConnectionString()
 	fmt.Println("DB Connection:", dbConn)
 
-	// 获取Redis密码
-	redisPass := GlobalConfig.Redis.Password
-	fmt.Println("Redis Password:", redisPass)
+	fmt.Println("GlobalConfig: ", GlobalConfig.Log.Level)
+
 }
--- main.go
+++ main.go
@@ -1,44 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"goutils/databaseconfig"
-	"goutils/logger"
-	"time"
+	"goutils/config"
 )
 
 func main() {
-	fmt.Println("this is a utils class with golang program.")
+	// fmt.Println("this is a utils class with golang program.")
 
-	// 初始化数据库配置
-	var config_db = databaseconfig.MysqlConfig{
-		Username: "senselink",
-		Password: "senselink_2018_local",
-		Port:     "3306",
-		Address:  "192.168.220.208",
-		Database: "bi_slink_base",
-	}
-	maxOpenConns, maxIdleConns := 20, 10
+	// // 初始化数据库配置
+	// var config_db = databaseconfig.MysqlConfig{
+	// 	Username: "senselink",
+	// 	Password: "senselink_2018_local",
+	// 	Port:     "3306",
+	// 	Address:  "192.168.220.208",
+	// 	Database: "bi_slink_base",
+	// }
+	// maxOpenConns, maxIdleConns := 20, 10
 
-	// 初始化 Log
-	filePath, filename := "./", "package_common"+time.Now().Format("2006-01-02")+".json"
-	Log := logger.InitLog(filePath, filename)
-	mysql_db, err := databaseconfig.NewMySQLDB(config_db, maxOpenConns, maxIdleConns)
+	// // 初始化 Log
+	// filePath, filename := "./", "package_common"+time.Now().Format("2006-01-02")+".json"
+	// Log := logger.InitLog(filePath, filename)
+	// mysql_db, err := databaseconfig.NewMySQLDB(config_db, maxOpenConns, maxIdleConns)
 
-	if err != nil {
-		Log.Info("初始化数据库错误： %s", err)
-	}
-
-	sql := "select * from t_user"
-	res, err := mysql_db.FetchAll(sql)
-
-	if err != nil {
-		Log.Info("初始化数据库错误： %s", err)
-	}
-
-	fmt.Println(res[0])
-	// for index, item := range res {
-	// 	fmt.Println(index, item)
+	// if err != nil {
+	// 	Log.Info("初始化数据库错误： %s", err)
 	// }
 
+	// sql := "select * from t_user"
+	// res, err := mysql_db.FetchAll(sql)
+
+	// if err != nil {
+	// 	Log.Info("初始化数据库错误： %s", err)
+	// }
+
+	// fmt.Println(res[0])
+	config.InitConfig()
+
+	config.ExampleUsage()
 }
